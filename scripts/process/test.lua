a88783fdfcf168cd7a7977e72dfe65ad4fe9492d
hunzsig <mzyhaohaoren@qq.com>
2024-02-15 11:01:06 +0800

    effect

--- scripts/process/test.lua
+++ scripts/process/test.lua
@@ -1,21 +1,22 @@
 local process = Process("test")
 
 process:onStart(function(this)
-
+    
     print("Hello, Lik!")
     echo("你好，Lik！")
-
+    
     sound.bgm("lik")
-
+    
     local bubble = this:bubble()
-    bubble.eff = Effect("Echo", 0, 0, 0, -1):rotateZ(270)
+    bubble.eff = Effect("Echo", 0, 0, 0, -1)
+    bubble.eff:rotateZ(270)
     bubble.unit = Unit(TPL_UNIT.DEMO, Player(1), 0, -100, 270)
     bubble.unit2 = Unit(TPL_UNIT.DEMO2, Player(2), 0, -100, 270)
 
 end)
 
 process:onOver(function(this)
-
+    
     sound.bgmStop()
 
 end)
